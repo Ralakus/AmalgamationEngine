a113b23a491d59e199f3749d2260da52a7d575f7
Mikhail <12schneide@gmail.com>
2018-07-05 16:53:47 -0400

    Clean Slate

--- Amalgamation/Core/Math/Math.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "MathDefines.hpp"
-#include "MathFunctions.hpp"
-#include "Vector/Vector2.hpp"
-#include "Vector/Vector3.hpp"
-#include "Vector/Vector4.hpp"
-#include "Quaternion/Quaternion.hpp"
-#include "Matrix/Matrix4x4.hpp"
--- Amalgamation/Core/Math/MathDefines.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#if !defined(AE_MATH_TYPE)
-	#define AE_MATH_TYPE float
-#endif
-#define AE_PI  3.14159265358979323846
-#define AE_TAU 6.28318530717958647692
